7554fea27b18b0908e815290dcf79ea381edbd45
Vi Vo <aaronvo118@gmail.com>
2022-03-20 12:15:15 -0500

    commit tree e29934cfb5ade6643984e9776663dcdca24ef017

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    BakedGood = BakedGood.by_price
+    BakedGood.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked_good = BakedGood.by_price.first
+    baked_good.to_json
+  end
 
 end
--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,9 @@
 class BakedGood < ActiveRecord::Base
   # add association macro here
+  belongs_to :bakery
+
+  def self.by_price
+    self.all.order(price: :desc)
+  end
+
 end
